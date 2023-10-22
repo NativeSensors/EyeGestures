9fdcfbbfba48851cd6de957f7113dd84d0a2bf96
Wallace <walas.piotr.prywatny@gmail.com>
2023-10-22 20:30:39 +0200

    disecting face into roi

--- main.py
+++ main.py
@@ -1,58 +1,36 @@
 import cv2
+import pickle
 
-#print("Before URL")
-cap = cv2.VideoCapture('rtsp://192.168.18.14:8080/h264.sdp')
-#print("After URL")
+class eyeFrame:
+    def __init__(self,eyes):
+        self.eyes = eyes
 
-# Load the cascade
+    def get(self):
+        return self.eyes
 
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
-# Read the input image
+class eyeFrameStorage:
+    def __init__(self):
+        self.frame = []
 
-def getEyes(gray):
-    # Detect faces
-    eyes = eye_cascade.detectMultiScale(gray, 1.1, 4)
-    # Draw rectangle around the faces
-    print(f"{len(eyes)} eyes detected")
-    eye_images = []
-    for (x, y, w, h) in eyes:
-        eye_images.append(gray[y:y+h,x:x+w])
-        # cv2.rectangle(gray, (x, y), (x+w, y+h), (255, 0, 0), 2)
-    return eye_images
+    def append(self,frame):
+        self.frame.append()
 
-def getFaces(gray):
-    # Detect faces
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-    # Draw rectangle around the faces
-    print(f"{len(faces)} faces detected")
-    faces_images = []
-    for (x, y, w, h) in faces:
-        faces_images.append(gray[y:y+h,x:x+w])
-        # cv2.rectangle(gray, (x, y), (x+w, y+h), (255, 0, 0), 2)
-    return faces_images
+    def get(self):
+        return self.frame
+
 
 if __name__ == "__main__":
-    while True:
+    framesStorage = eyeFrameStorage()
 
-        #print('About to start the Read command')
-        ret, frame = cap.read()
-        #print('Running..')
-        
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = getFaces(gray)
-        
-        for face in faces:
-            eyes = getEyes(face)
-        
+    with open('recording/data.pkl', 'rb') as file:
+        framesStorage = pickle.load(file)
 
-            # find the smallest difference
-            for i,eye in enumerate(eyes):
+    for n, frame in enumerate(framesStorage.get()):
 
-                cv2.imshow(f'eye_{i}', eye)
+        for i,eye in enumerate(frame.get()):
+            cv2.imshow(f'eye_{i}', eye)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(10) == ord('q'):
             break
 
-    cap.release()
     cv2.destroyAllWindows()
---
+++ record.py
@@ -0,0 +1,89 @@
+import cv2
+import time
+import pickle 
+
+#print("Before URL")
+cap = cv2.VideoCapture('rtsp://192.168.18.14:8080/h264.sdp')
+#print("After URL")
+
+# Load the cascade
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+# Read the input image
+
+class eyeFrame:
+    def __init__(self,eyes):
+        self.eyes = eyes
+
+    def get(self):
+        return self.eyes
+
+class eyeFrameStorage:
+    def __init__(self):
+        self.frame = []
+
+    def append(self,frame):
+        self.frame.append(frame)
+
+    def get(self):
+        return self.frame
+
+def getEyes(gray):
+    # Detect faces
+    eyes = eye_cascade.detectMultiScale(gray, 1.1, 4)
+    # Draw rectangle around the faces
+    eye_images = []
+    for (x, y, w, h) in eyes:
+        eye_images.append(gray[y:y+h,x:x+w])
+        # cv2.rectangle(gray, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    return eye_images
+
+def getFaces(gray):
+    # Detect faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    # Draw rectangle around the faces
+    faces_images = []
+    for (x, y, w, h) in faces:
+        faces_images.append(gray[y:y+h,x:x+w])
+        # cv2.rectangle(gray, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    return faces_images
+
+framesStorage = eyeFrameStorage()
+
+if __name__ == "__main__":
+
+    # Get the current time
+    while True:
+
+        #print('About to start the Read command')
+        ret, frame = cap.read()
+        #print('Running..')
+        
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = getFaces(gray)
+        
+        for face in faces:
+            left_face = face[:int(face.shape[0]/2),:int(face.shape[0]/2)]
+            right_face = face[:int(face.shape[0]/2),int(face.shape[0]/2):]
+            leftEye = eyeFrame(getEyes(left_face))
+            rightEye = eyeFrame(getEyes(right_face))
+            # ef = eyeFrame(getEyes(face)) 
+            # framesStorage.append(ef)
+
+            for i,eye in enumerate(leftEye.get()):
+                cv2.imshow(f'left_eye_{i}', eye)
+
+            for i,eye in enumerate(rightEye.get()):
+                cv2.imshow(f'right_eye_{i}', eye)
+            # cv2.imshow(f'left_face', left_face)
+            # cv2.imshow(f'right_face', right_face)
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    with open('recording/data.pkl', 'wb') as file:
+        pickle.dump(framesStorage, file)
+
+    cap.release()
+    cv2.destroyAllWindows()
