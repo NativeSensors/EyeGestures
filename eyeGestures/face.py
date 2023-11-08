a4ebf4feb893222d300f59625422d2c26643b6db
Wallace <walas.piotr.prywatny@gmail.com>
2023-11-08 02:13:40 +0100

    Adding face and face detector

---
+++ eyeGestures/face.py
@@ -0,0 +1,62 @@
+import cv2
+import dlib
+import math
+import numpy as np
+import eyeGestures.utils as utils
+import eyeGestures.eye as eye
+
+class FaceFinder:
+
+    def __init__(self):
+        self.facePredictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+        self.faceDetector = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+        
+    def find(self,image):
+        try:
+            (x, y, w, h) = self.faceDetector.detectMultiScale(image, 1.1, 9)[0]
+            face = self.facePredictor(image, dlib.rectangle(x, y, x+w, y+h))
+        
+            return Face(image,face)
+        except Exception as e:
+            print(f"Exception: {e}")
+            return None
+
+class Face:
+
+    def __init__(self,image,face):
+        self.face = face
+
+        self.landmarks = self._landmarks(self.face)
+        self._process(image,self.face)
+
+    def getLeftEye(self):
+        return self.eyeLeft.getLandmarks()
+
+    def getRightEye(self):
+        return self.eyeRight.getLandmarks()
+
+    def getLeftPupil(self):
+        return self.eyeLeft.getPupil()
+
+    def getRightPupil(self):
+        return self.eyeRight.getPupil()
+
+    def getLandmarks(self):
+        return self.landmarks
+
+    def _landmarks(self,face):
+        landmarks = np.zeros((face.num_parts, 2), dtype=np.dtype)
+        for i in range(0, face.num_parts):
+            landmarks[i] = (face.part(i).x, face.part(i).y)
+
+        return landmarks
+
+    def _process(self,image,face):
+        rect = face.rect
+        
+        lt_corner = (rect.left(),rect.top())
+        rb_corner = (rect.right(),rect.bottom())
+
+        self.eyeLeft  = eye.Eye(image,self.landmarks,0)
+        self.eyeRight = eye.Eye(image,self.landmarks,1)
+        
--- pupilDetection.py
+++ pupilDetection.py
@@ -7,17 +7,29 @@ import random
 import threading
 import numpy as np
 from typing import Callable, Tuple
-from eyeGestures.eye import Eye
 from eyeGestures.utils import VideoCapture
+from eyeGestures.face import FaceFinder
+
+def showEyes(image):
+
+    if face is not None:
+        cv2.circle(frame,face.getLeftPupil(),2,(0,0,255),1)
+        for point in face.getLeftEye():
+            cv2.circle(frame,point,2,(0,255,0),1)
+
+        cv2.circle(frame,face.getRightPupil(),2,(0,0,255),1)
+        for point in face.getRightEye():
+            cv2.circle(frame,point,2,(0,255,0),1)
+
 
 if __name__ == "__main__":    
     print("opening stream")
-    # vid = VideoCapture('recording/calibrationData1.pkl')
-    vid = VideoCapture('rtsp://192.168.18.30:8080/h264.sdp')
+    vid = VideoCapture('recording/calibrationData1.pkl')
+    # vid = VideoCapture('rtsp://192.168.18.30:8080/h264.sdp')
     
+    faceFinder = FaceFinder()
+
     print("loading detectors")
-    facePredictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
-    faceDetector = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
     # use half of the data for fitting
     pFrame = None
     flow = None
@@ -28,34 +40,12 @@ if __name__ == "__main__":
         ret, frame = vid.read()
         
         gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-        try:
-            (x, y, w, h) = faceDetector.detectMultiScale(gray, 1.1, 9)[0]
-            face = facePredictor(gray, dlib.rectangle(x, y, x+w, y+h))
-            rect = face.rect
-            lt_corner = (rect.left(),rect.top())
-            rb_corner = (rect.right(),rect.bottom())
 
-            landmarks = np.zeros((face.num_parts, 2), dtype=np.dtype)
-            for i in range(0, face.num_parts):
-                landmarks[i] = (face.part(i).x, face.part(i).y)
-
-            eyeLeft  = Eye(gray,landmarks,0)
-            eyeRight = Eye(gray,landmarks,1)
-
-            cv2.circle(frame,eyeLeft.getPupil(),2,(0,0,255),1)
-            for point in eyeLeft.getLandmarks():
-                cv2.circle(frame,point,2,(0,255,0),1)
-
-            cv2.circle(frame,eyeRight.getPupil(),2,(0,0,255),1)
-            for point in eyeRight.getLandmarks():
-                cv2.circle(frame,point,2,(0,255,0),1)
-
-            cv2.rectangle(frame,lt_corner,rb_corner,(0,0,255),1)
-        except Exception as e:
-            print(f"Caught exception: {e}")
-            pass
-
+        face = faceFinder.find(gray)
+        
+        showEyes(frame)
         cv2.imshow("frame",frame)
+    
         if cv2.waitKey(10) == ord('q'):
             run = False
             break         
