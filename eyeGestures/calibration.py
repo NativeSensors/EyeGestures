ceaf8df5bcf9058a831bfefeeb2bf3585ed1e3ad
Wallace <piotr.walas.prywatny@gmail.com>
2024-04-04 00:42:53 +0200

    running pylint adding docstrings etc

--- eyeGestures/calibration.py
+++ eyeGestures/calibration.py
@@ -1,30 +1,29 @@
 
-import time 
-import math
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
+import time
 
 class Calibrator:
+    """Class representing a calibration process"""
 
-    def __init__(self,width,height,time = 60):
+    def __init__(self,width,height,_time = 60):
         self.width = width
         self.height = height
-        self.time = time
+        self.time = _time
 
         self.run = False
         self.t_start = 0
 
-        self.onFinish = None
+        self.on_finish = None
+        self.points = []
 
-    def start(self,onFinish):
+    def start(self,on_finish):
+        """Function starting calibration."""
         self.t_start = time.time()
         self.run = True
 
-        self.onFinish = onFinish
+        self.on_finish = on_finish
 
-    def getTrainingPoint(self):
+    def get_training_point(self):
+        """Returns training point for calibration."""
 
         if not self.run:
             return self.points[0]
@@ -36,8 +35,8 @@ class Calibrator:
             if(index < 0 or index >= len(self.points)):
                 self.run = False
 
-                if not self.onFinish is None:
-                    self.onFinish()
+                if not self.on_finish is None:
+                    self.on_finish()
                     return self.points[0]
                 
             return self.points[index]
--- eyeGestures/eye.py
+++ eyeGestures/eye.py
@@ -1,13 +1,14 @@
+"""Module providing a extraction of eye from face object."""
+
 import cv2
-import math
 import numpy as np
 import mediapipe as mp
-import eyeGestures.pupil as pupil
 from scipy.optimize import fsolve
-from eyeGestures.utils import Buffor
 
 # Function to fit a quadratic curve and find intersections
 def fit_curve(p1, p2):
+    """Function finding polynomial between two points p1 and p2."""
+
     # Fit a quadratic curve (ax^2 + bx + c) through the points
     coefficients = np.polyfit([p1[0], p2[0]], [p1[1], p2[1]], 2)
     curve = np.poly1d(coefficients)
@@ -16,20 +17,24 @@ def fit_curve(p1, p2):
 
 # Function to find intersections with y = 0
 def find_intersections(curve, points):
+    """Function finding intersection between curve and point ."""
+
     def intersection(x):
         return curve(x)
 
     intersection_points = fsolve(intersection, 0)
     return (intersection_points,0)
 
-def getCurves(points, reference_point):
+def get_curves(points, reference_point):
+    """Function finding curve for points and reference point."""
+
     segments = []
     intersection_points = []
-    for i in range(len(points)):
+    for i,_ in enumerate(points):
         p1, p2 = points[i], points[(i + 1 ) % len(points)]
         if (p1[1] - reference_point) * (p2[1] - reference_point) < 0:
             curve = fit_curve(p1, p2)
-            
+
             segment_x_range = np.linspace(p1[0], p2[0], 100)
             segments.append((curve,segment_x_range))
             x = find_intersections(curve,np.array([p1, p2]))
@@ -37,25 +42,28 @@ def getCurves(points, reference_point):
 
     return segments,intersection_points
 
-def getIntersections(points, reference_point):
-    _, intersetions = getCurves(points, reference_point)
+def get_intersections(points, reference_point):
+    """Function intersection between point and reference point."""
+
+    _, intersetions = get_curves(points, reference_point)
     return np.array(intersetions)
 
-class Eye:    
+class Eye:
+    """Class storing data related and representing a eye"""     
     LEFT_EYE_KEYPOINTS  = np.array(list(mp.solutions.face_mesh.FACEMESH_LEFT_EYE))[:,0]
     RIGHT_EYE_KEYPOINTS = np.array(list(mp.solutions.face_mesh.FACEMESH_RIGHT_EYE))[:,0]
     LEFT_EYE_IRIS_KEYPOINT  = []
     RIGHT_EYE_IRIS_KEYPOINT = []
     LEFT_EYE_PUPIL_KEYPOINT  = [473]
     RIGHT_EYE_PUPIL_KEYPOINT = [468]
-        
+
     # LEFT_EYE_KEYPOINTS = [36, 37, 38, 39, 40, 41] # keypoint indices for left eye
     # RIGHT_EYE_KEYPOINTS = [42, 43, 44, 45, 46, 47] # keypoint indices for right eye
 
     scale = (150,100)
 
     def __init__(self, side : int):
-        
+
         # check if eye is left or right
         if side == 1:
             self.side = "right"
@@ -63,10 +71,25 @@ class Eye:
         elif side == 0:
             self.side = "left"
             self.pupil_index = self.LEFT_EYE_PUPIL_KEYPOINT
-        
+
+        self.x = 0
+        self.y = 0
+        self.width = 0
+        self.height = 0
+        self.center_x = 0
+        self.center_y = 0
+        self.image = None
+        self.pupil = None
+        self.offset = None
+        self.region = None
+        self.cut_image = None
+        self.landmarks = None
+
         # self._process(self.image,self.region)
 
     def update(self,image : np.ndarray, landmarks : list, offset : np.ndarray):
+        """function updating data stored inside eye object"""
+
         self.image = image
         self.offset = offset
         self.landmarks = landmarks
@@ -74,58 +97,73 @@ class Eye:
         # check if eye is left or right
         if self.side == "right":
             self.region = np.array(landmarks[self.RIGHT_EYE_KEYPOINTS], dtype=np.int32)
-        
+
         elif self.side == "left":
             self.region = np.array(landmarks[self.LEFT_EYE_KEYPOINTS], dtype=np.int32)
-        
+
         self.pupil = landmarks[self.pupil_index][0]
         self._process(self.image,self.region)
 
     def getCenter(self):
+        """function returning center of eye"""
+
         return (self.center_x,self.center_y)
 
     def getPos(self):
+        """function returning position of eye in the image"""
+
         return (self.x,self.y)
 
     def getPupil(self):
+        """function returning pupil object"""
+
         # return self.pupil.getCoords()
         return self.pupil
-    
+
     def getBlink(self):
+        """function returning blink event"""
         # return self.pupil.getCoords()
         return (self.height) <= 3 # 2x margin 
 
     def getImage(self):
+        """function returning image of the eye cut from the entire face image"""
+
         # TODO: draw additional parameters
         return self.cut_image
 
     def getGaze(self,gaze_buffor,y_correction=0,x_correction=0):
+        """function returning gaze position"""
+
         # pupilCoords = self.pupil.getCoords()
         center = np.array((self.center_x,self.center_y)) - self.offset
-        
+
         region_corrected = self.region - self.offset
         pupil_corrected = self.pupil - self.offset
 
         vectors = region_corrected  - center
         pupil = pupil_corrected - center
-        
+
         vectors = vectors - pupil
         gaze_vector = np.zeros((2))
-        
+
         gaze_vector[1] = np.sum(vectors, axis=0)[1] * 10 - y_correction
         gaze_vector[0] = -np.sum(vectors, axis=0)[0] * 10 - x_correction
-        
+
         # print("gaze_vector: ",gaze_vector)
-        gaze_buffor.add(gaze_vector)   
+        gaze_buffor.add(gaze_vector)
         return gaze_buffor.getAvg()
-        
+
     def getOpenness(self):
+        """function returning eye openness"""
+
         return self.height/2 
 
     def getLandmarks(self):
+        """function returning eye landmarks"""
+
         return self.region 
 
-    def _process(self,image,region):
+    def _process(self,image,region):        
         h, w, _ = image.shape
 
         mask = np.full((h, w), 0, dtype=np.uint8) 
@@ -133,30 +171,30 @@ class Eye:
         cv2.fillPoly(mask, [region], 0)
 
         masked_image = cv2.bitwise_not(background, cv2.cvtColor(image.copy(), cv2.COLOR_BGR2GRAY), mask=mask)
-        
+
         margin = 2
         min_x = np.min(region[:,0]) - margin
         max_x = np.max(region[:,0]) + margin
         min_y = np.min(region[:,1]) - margin
         max_y = np.max(region[:,1]) + margin
-        
+
         self.x = min_x
         self.y = min_y
-        
+
         self.width  = np.max(region[:,0]) - np.min(region[:,0])
         self.height = np.max(region[:,1]) - np.min(region[:,1])
-        
+
         self.center_x = (min_x + max_x)/2
         self.center_y = (min_y + max_y)/2
 
         # HACKETY_HACK: 
         self.pupil[1] = np.min(region[:,1])
-    
-        # self.intersection = getIntersections(region,self.center_y)
+
+        # self.intersection = get_intersections(region,self.center_y)
         self.cut_image = masked_image[min_y:max_y,min_x:max_x]
         # print(f"here: {self.cut_image.shape,min_y,max_y,min_x,max_x}")
         self.cut_image = cv2.cvtColor(self.cut_image, cv2.COLOR_GRAY2BGR)
-        
+
         for point in self.region:
             point = point - (min_x,min_y)
             cv2.circle(self.cut_image,point.astype(int),1,(255,0,0,150),1)
@@ -167,14 +205,11 @@ class Eye:
         cv2.circle(self.cut_image,pupil.astype(int),1,(0,255,0,150),1)
 
         self.cut_image = cv2.resize(self.cut_image,self.scale)
-        
-        # save cut_image to buffor and get avg from previous buffors 
+
+        # save cut_image to buffor and get avg from previous buffors
         # self.eyeBuffer.add(self.cut_image)
-        # self.cut_image = np.array(self.eyeBuffer.getAvg(), dtype=np.uint8) 
-            
+        # self.cut_image = np.array(self.eyeBuffer.getAvg(), dtype=np.uint8)
+
         # LEGACY
         # org_scale = (max_x - min_x,max_y - min_y)
         # self.pupil = pupil.Pupil(self.cut_image, min_x, min_y, self.scale, org_scale)
-        
-
-            
