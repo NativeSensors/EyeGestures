1252ead7b46b68bae73cb09bcbf9658eb8f69912
PeterWaIIace <piotr.walas.prywatny@gmail.com>
2024-10-23 16:49:00 +0200

    changing to Lasso regrerssion

--- examples/simple_example_v2.py
+++ examples/simple_example_v2.py
@@ -15,6 +15,9 @@ screen_height = screen_info.current_h
 # Set up the screen
 screen = pygame.display.set_mode((screen_width, screen_height), pygame.FULLSCREEN)
 pygame.display.set_caption("EyeGestures v2 example")
+font_size = 48
+bold_font = pygame.font.Font(None, font_size)
+bold_font.set_bold(True)  # Set the font to bold
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(f'{dir_path}/..')
@@ -41,6 +44,7 @@ RED = (255, 0, 100)
 BLUE = (100, 0, 255)
 GREEN = (0, 255, 0)
 BLANK = (0,0,0)
+WHITE = (255, 255, 255)
 
 clock = pygame.time.Clock()
 
@@ -79,14 +83,17 @@ while running:
         text_surface = my_font.render(f'{event.fixation}', False, (0, 0, 0))
         screen.blit(text_surface, (0,0))
         if calibrate:
-            if calibration.point[0] != prev_x and calibration.point[1] != prev_y:
+            if calibration.point[0] != prev_x or calibration.point[1] != prev_y:
                 iterator += 1
                 prev_x = calibration.point[0]
                 prev_y = calibration.point[1]
             # pygame.draw.circle(screen, GREEN, fit_point, calibration_radius)
             pygame.draw.circle(screen, BLUE, calibration.point, calibration.acceptance_radius)
+            text_surface = bold_font.render(f"{iterator}/{25}", True, WHITE)
+            text_square = text_surface.get_rect(center=calibration.point)
+            screen.blit(text_surface, text_square)
         else:
-            pygame.draw.circle(screen, BLANK, calibration.point, calibration.acceptance_radius)
+            pass
         pygame.draw.circle(screen, RED, event.point, 50)
     pygame.display.flip()
 
